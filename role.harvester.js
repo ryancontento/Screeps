9475f1c28d121180a611f391a4a4835829c4f150
Ryan Contento <rkc61595@gmail.com>
2023-11-25 07:10:03 -0500

    WIP

--- role.harvester.js
+++ role.harvester.js
@@ -3,48 +3,10 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.store.getFreeCapacity() > 0) {
-
-            Room.source.sourcePriority  = function(source) {
-                let priority;
-                if (source.ticksToRegeneration == undefined) {
-                    priority = 10;
-                } else if (source.energy == 0) {
-                    priority = 0;
-                } else {
-                    priority = source.energy / source.ticksToRegeneration;
-                }
-                if (priority > 0 && source.ticksToRegeneration < 150) {
-                    priority = priority * (1 + (150 - source.ticksToRegeneration)/250);
-                    if (source.ticksToRegeneration < 70) {
-                        priority = priority + (70 - source.ticksToRegeneration)/10;
-                    }
-                }
-                return priority;
-            };
-
-            let sources = creep.room.find(FIND_SOURCES);
-            let used;
-
-            let switchSource = _.random(0, 4) == 0;
-            if (Sources.sourcePriority(sources[1]) > Sources.sourcePriority(sources[0])) {
-            if (switchSource) {
-                used = sources[0];
-            } else {
-                used = sources[1];
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-            } else {
-            if (switchSource) {
-                used = sources[1];
-            } else {
-                used = sources[0];
-            }
-
-            creep.moveTo(sources[used], {visualizePathStyle: {stroke: '#ffaa00'}});
-}
-            // var sources = creep.room.find(FIND_SOURCES);
-            // if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            //     creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            // }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
@@ -65,3 +27,40 @@ var roleHarvester = {
 };
 
 module.exports = roleHarvester;
+
+ // Room.source.sourcePriority  = function(source) {
+            //     let priority;
+            //     if (source.ticksToRegeneration == undefined) {
+            //         priority = 10;
+            //     } else if (source.energy == 0) {
+            //         priority = 0;
+            //     } else {
+            //         priority = source.energy / source.ticksToRegeneration;
+            //     }
+            //     if (priority > 0 && source.ticksToRegeneration < 150) {
+            //         priority = priority * (1 + (150 - source.ticksToRegeneration)/250);
+            //         if (source.ticksToRegeneration < 70) {
+            //             priority = priority + (70 - source.ticksToRegeneration)/10;
+            //         }
+            //     }
+            //     return priority;
+            // };
+
+            // let sources = creep.room.find(FIND_SOURCES);
+            // let used;
+
+            // let switchSource = _.random(0, 4) == 0;
+            // if (Sources.sourcePriority(sources[1]) > Sources.sourcePriority(sources[0])) {
+            // if (switchSource) {
+            //     used = sources[0];
+            // } else {
+            //     used = sources[1];
+            // }
+            // } else {
+            // if (switchSource) {
+            //     used = sources[1];
+            // } else {
+            //     used = sources[0];
+            // }
+
+            // creep.moveTo(sources[used], {visualizePathStyle: {stroke: '#ffaa00'}});
