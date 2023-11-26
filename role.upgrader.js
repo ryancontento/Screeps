32c5c968c9c529084c100e54bf0265b5068f6eab
Ryan Contento <rkc61595@gmail.com>
2023-11-26 07:36:37 -0500

    logic to alternate sources on all current creeps.

--- role.upgrader.js
+++ role.upgrader.js
@@ -19,8 +19,21 @@ var roleUpgrader = {
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+
+            var sources = creep.room.find(FIND_SOURCES);
+
+			let switchSource = _.random(0, 4) == 0;
+            let used;
+            if (switchSource) {
+                used = sources[0];
+            } else {
+                used = sources[1];
+            }
+
+            console.log(`Source used: ${used}`);
+
+            if(creep.harvest(used) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(used, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 	}
